dcf1920c9d4d7d255b7ee2cb136ef207a2be07b6
Commandmon <117977960+Commandmon@users.noreply.github.com>
2020-09-07 22:51:58 +0000

    Release 1.2.1

---
+++ examples/v1/usage-metering/GetUsageCWS.rb
@@ -0,0 +1,6 @@
+
+# Get hourly usage for cloud workload security returns "OK" response
+
+require "datadog_api_client"
+api_instance = DatadogAPIClient::V1::UsageMeteringAPI.new
+p api_instance.get_usage_cws("2021-11-11T11:11:11.111+00:00")
