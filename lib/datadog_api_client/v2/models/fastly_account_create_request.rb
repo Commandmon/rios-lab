eaea6f01b4b1abc92652679ce84151ba20041496
Commandmon <117977960+Commandmon@users.noreply.github.com>
2020-09-07 06:46:45 +0000

    Edited wiki page Asterisk through web user interface.

--- lib/datadog_api_client/v2/models/fastly_account_create_request.rb
+++ lib/datadog_api_client/v2/models/fastly_account_create_request.rb
@@ -1,3 +1,4 @@
+
 =begin
 #Datadog API V2 Collection
 
@@ -17,30 +18,22 @@ require 'date'
 require 'time'
 
 module DatadogAPIClient::V2
-  # Service documents.
-  class ServiceDefinitionV2Doc
+  # Payload schema when adding a Fastly account.
+  class FastlyAccountCreateRequest
     include BaseGenericModel
 
     # Whether the object has unparsed attributes
     # @!visibility private
     attr_accessor :_unparsed
 
-    # Document name.
-    attr_reader :name
-
-    # Document provider.
-    attr_accessor :provider
-
-    # Document URL.
-    attr_reader :url
+    # Data object for creating a Fastly account.
+    attr_reader :data
 
     # Attribute mapping from ruby-style variable name to JSON key.
     # @!visibility private
     def self.attribute_map
       {
-        :'name' => :'name',
-        :'provider' => :'provider',
-        :'url' => :'url'
+        :'data' => :'data'
       }
     end
 
@@ -48,9 +41,7 @@ module DatadogAPIClient::V2
     # @!visibility private
     def self.openapi_types
       {
-        :'name' => :'String',
-        :'provider' => :'String',
-        :'url' => :'String'
+        :'data' => :'FastlyAccountCreateRequestData'
       }
     end
 
@@ -59,27 +50,19 @@ module DatadogAPIClient::V2
     # @!visibility private
     def initialize(attributes = {})
       if (!attributes.is_a?(Hash))
-        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V2::ServiceDefinitionV2Doc` initialize method"
+        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V2::FastlyAccountCreateRequest` initialize method"
       end
 
       # check to see if the attribute exists and convert string to symbol for hash key
       attributes = attributes.each_with_object({}) { |(k, v), h|
         if (!self.class.attribute_map.key?(k.to_sym))
-          fail ArgumentError, "`#{k}` is not a valid attribute in `DatadogAPIClient::V2::ServiceDefinitionV2Doc`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
+          fail ArgumentError, "`#{k}` is not a valid attribute in `DatadogAPIClient::V2::FastlyAccountCreateRequest`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
         end
         h[k.to_sym] = v
       }
 
-      if attributes.key?(:'name')
-        self.name = attributes[:'name']
-      end
-
-      if attributes.key?(:'provider')
-        self.provider = attributes[:'provider']
-      end
-
-      if attributes.key?(:'url')
-        self.url = attributes[:'url']
+      if attributes.key?(:'data')
+        self.data = attributes[:'data']
       end
     end
 
@@ -87,29 +70,18 @@ module DatadogAPIClient::V2
     # @return true if the model is valid
     # @!visibility private
     def valid?
-      return false if @name.nil?
-      return false if @url.nil?
+      return false if @data.nil?
       true
     end
 
     # Custom attribute writer method with validation
-    # @param name [Object] Object to be assigned
+    # @param data [Object] Object to be assigned
     # @!visibility private
-    def name=(name)
-      if name.nil?
-        fail ArgumentError, 'invalid value for "name", name cannot be nil.'
+    def data=(data)
+      if data.nil?
+        fail ArgumentError, 'invalid value for "data", data cannot be nil.'
       end
-      @name = name
-    end
-
-    # Custom attribute writer method with validation
-    # @param url [Object] Object to be assigned
-    # @!visibility private
-    def url=(url)
-      if url.nil?
-        fail ArgumentError, 'invalid value for "url", url cannot be nil.'
-      end
-      @url = url
+      @data = data
     end
 
     # Checks equality by comparing each attribute.
@@ -118,16 +90,14 @@ module DatadogAPIClient::V2
     def ==(o)
       return true if self.equal?(o)
       self.class == o.class &&
-          name == o.name &&
-          provider == o.provider &&
-          url == o.url
+          data == o.data
     end
 
     # Calculates hash code according to all attributes.
     # @return [Integer] Hash code
     # @!visibility private
     def hash
-      [name, provider, url].hash
+      [data].hash
     end
   end
 end
--- lib/datadog_api_client/v2/models/metric_bulk_tag_config_status_attributes.rb
+++ lib/datadog_api_client/v2/models/metric_bulk_tag_config_status_attributes.rb
@@ -1,3 +1,4 @@
+
 =begin
 #Datadog API V2 Collection
 
@@ -17,30 +18,30 @@ require 'date'
 require 'time'
 
 module DatadogAPIClient::V2
-  # Service documents.
-  class ServiceDefinitionV2Doc
+  # Optional attributes for the status of a bulk tag configuration request.
+  class MetricBulkTagConfigStatusAttributes
     include BaseGenericModel
 
     # Whether the object has unparsed attributes
     # @!visibility private
     attr_accessor :_unparsed
 
-    # Document name.
-    attr_reader :name
+    # A list of account emails to notify when the configuration is applied.
+    attr_accessor :emails
 
-    # Document provider.
-    attr_accessor :provider
+    # The status of the request.
+    attr_accessor :status
 
-    # Document URL.
-    attr_reader :url
+    # A list of tag names to apply to the configuration.
+    attr_accessor :tags
 
     # Attribute mapping from ruby-style variable name to JSON key.
     # @!visibility private
     def self.attribute_map
       {
-        :'name' => :'name',
-        :'provider' => :'provider',
-        :'url' => :'url'
+        :'emails' => :'emails',
+        :'status' => :'status',
+        :'tags' => :'tags'
       }
     end
 
@@ -48,9 +49,9 @@ module DatadogAPIClient::V2
     # @!visibility private
     def self.openapi_types
       {
-        :'name' => :'String',
-        :'provider' => :'String',
-        :'url' => :'String'
+        :'emails' => :'Array<String>',
+        :'status' => :'String',
+        :'tags' => :'Array<String>'
       }
     end
 
@@ -59,27 +60,31 @@ module DatadogAPIClient::V2
     # @!visibility private
     def initialize(attributes = {})
       if (!attributes.is_a?(Hash))
-        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V2::ServiceDefinitionV2Doc` initialize method"
+        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V2::MetricBulkTagConfigStatusAttributes` initialize method"
       end
 
       # check to see if the attribute exists and convert string to symbol for hash key
       attributes = attributes.each_with_object({}) { |(k, v), h|
         if (!self.class.attribute_map.key?(k.to_sym))
-          fail ArgumentError, "`#{k}` is not a valid attribute in `DatadogAPIClient::V2::ServiceDefinitionV2Doc`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
+          fail ArgumentError, "`#{k}` is not a valid attribute in `DatadogAPIClient::V2::MetricBulkTagConfigStatusAttributes`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
         end
         h[k.to_sym] = v
       }
 
-      if attributes.key?(:'name')
-        self.name = attributes[:'name']
+      if attributes.key?(:'emails')
+        if (value = attributes[:'emails']).is_a?(Array)
+          self.emails = value
+        end
       end
 
-      if attributes.key?(:'provider')
-        self.provider = attributes[:'provider']
+      if attributes.key?(:'status')
+        self.status = attributes[:'status']
       end
 
-      if attributes.key?(:'url')
-        self.url = attributes[:'url']
+      if attributes.key?(:'tags')
+        if (value = attributes[:'tags']).is_a?(Array)
+          self.tags = value
+        end
       end
     end
 
@@ -87,29 +92,7 @@ module DatadogAPIClient::V2
     # @return true if the model is valid
     # @!visibility private
     def valid?
-      return false if @name.nil?
-      return false if @url.nil?
       true
-    end
-
-    # Custom attribute writer method with validation
-    # @param name [Object] Object to be assigned
-    # @!visibility private
-    def name=(name)
-      if name.nil?
-        fail ArgumentError, 'invalid value for "name", name cannot be nil.'
-      end
-      @name = name
-    end
-
-    # Custom attribute writer method with validation
-    # @param url [Object] Object to be assigned
-    # @!visibility private
-    def url=(url)
-      if url.nil?
-        fail ArgumentError, 'invalid value for "url", url cannot be nil.'
-      end
-      @url = url
     end
 
     # Checks equality by comparing each attribute.
@@ -118,16 +101,16 @@ module DatadogAPIClient::V2
     def ==(o)
       return true if self.equal?(o)
       self.class == o.class &&
-          name == o.name &&
-          provider == o.provider &&
-          url == o.url
+          emails == o.emails &&
+          status == o.status &&
+          tags == o.tags
     end
 
     # Calculates hash code according to all attributes.
     # @return [Integer] Hash code
     # @!visibility private
     def hash
-      [name, provider, url].hash
+      [emails, status, tags].hash
     end
   end
 end
