04a9ff72d093d529a11ea50d665eadffddb4b3cd
Commandmon <117977960+Commandmon@users.noreply.github.com>
2020-10-24 12:34:48 +0000

    Initial Home page

--- lib/datadog_api_client/v1/models/api_key_list_response.rb
+++ lib/datadog_api_client/v1/models/api_key_list_response.rb
@@ -38,4 +38,56 @@ module DatadogAPIClient::V1
 
     # Attribute type mapping.
     # @!visibility private
-    def self.openapi_ty
+    def self.openapi_types
+      {
+        :'api_keys' => :'Array<ApiKey>'
+      }
+    end
+
+    # Initializes the object
+    # @param attributes [Hash] Model attributes in the form of hash
+    # @!visibility private
+    def initialize(attributes = {})
+      if (!attributes.is_a?(Hash))
+        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V1::ApiKeyListResponse` initialize method"
+      end
+
+      # check to see if the attribute exists and convert string to symbol for hash key
+      attributes = attributes.each_with_object({}) { |(k, v), h|
+        if (!self.class.attribute_map.key?(k.to_sym))
+          fail ArgumentError, "`#{k}` is not a valid attribute in `DatadogAPIClient::V1::ApiKeyListResponse`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
+        end
+        h[k.to_sym] = v
+      }
+
+      if attributes.key?(:'api_keys')
+        if (value = attributes[:'api_keys']).is_a?(Array)
+          self.api_keys = value
+        end
+      end
+    end
+
+    # Check to see if the all the properties in the model are valid
+    # @return true if the model is valid
+    # @!visibility private
+    def valid?
+      true
+    end
+
+    # Checks equality by comparing each attribute.
+    # @param o [Object] Object to be compared
+    # @!visibility private
+    def ==(o)
+      return true if self.equal?(o)
+      self.class == o.class &&
+          api_keys == o.api_keys
+    end
+
+    # Calculates hash code according to all attributes.
+    # @return [Integer] Hash code
+    # @!visibility private
+    def hash
+      [api_keys].hash
+    end
+  end
+end
