147f09fded467abd6c7e79033b2181d62688a98d
Commandmon <117977960+Commandmon@users.noreply.github.com>
2020-11-24 12:16:43 +0000

    added missing icon

---
+++ run-tests.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+function finish {
+    rm -rf rerun.txt failed.txt
+}
+trap finish EXIT
+
+bundle install
+RSPEC_RESULT=0
+if [ "${RECORD:-false}" == "false" ]; then
+    bundle exec rspec
+    RSPEC_RESULT=$?
+fi
+
+bundle exec cucumber -f rerun -o rerun.txt -f pretty
+CUCUMBER_RESULT=$?
+if [ "$RERECORD_FAILED_TESTS" == "true" ] && [ "$CUCUMBER_RESULT" -ne "0" ]; then
+    RECORD=true bundle exec cucumber @rerun.txt
+    CUCUMBER_RESULT=$?
+fi
+
+exit "$(($RSPEC_RESULT+$CUCUMBER_RESULT))"
