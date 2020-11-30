e339e0824596ee3a7a3ba582d2311ed5b331e0a3
Commandmon <117977960+Commandmon@users.noreply.github.com>
2020-11-30 17:40:01 +0000

    remove unused comments

--- lib/datadog_api_client/v1/models/alert_value_widget_definition.rb
+++ lib/datadog_api_client/v1/models/alert_value_widget_definition.rb
@@ -32,4 +32,152 @@ module DatadogAPIClient::V1
     attr_accessor :precision
 
     # How to align the text on the widget.
- 
+    attr_accessor :text_align
+
+    # Title of the widget.
+    attr_accessor :title
+
+    # How to align the text on the widget.
+    attr_accessor :title_align
+
+    # Size of value in the widget.
+    attr_accessor :title_size
+
+    # Type of the alert value widget.
+    attr_reader :type
+
+    # Unit to display with the value.
+    attr_accessor :unit
+
+    # Attribute mapping from ruby-style variable name to JSON key.
+    # @!visibility private
+    def self.attribute_map
+      {
+        :'alert_id' => :'alert_id',
+        :'precision' => :'precision',
+        :'text_align' => :'text_align',
+        :'title' => :'title',
+        :'title_align' => :'title_align',
+        :'title_size' => :'title_size',
+        :'type' => :'type',
+        :'unit' => :'unit'
+      }
+    end
+
+    # Attribute type mapping.
+    # @!visibility private
+    def self.openapi_types
+      {
+        :'alert_id' => :'String',
+        :'precision' => :'Integer',
+        :'text_align' => :'WidgetTextAlign',
+        :'title' => :'String',
+        :'title_align' => :'WidgetTextAlign',
+        :'title_size' => :'String',
+        :'type' => :'AlertValueWidgetDefinitionType',
+        :'unit' => :'String'
+      }
+    end
+
+    # Initializes the object
+    # @param attributes [Hash] Model attributes in the form of hash
+    # @!visibility private
+    def initialize(attributes = {})
+      if (!attributes.is_a?(Hash))
+        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V1::AlertValueWidgetDefinition` initialize method"
+      end
+
+      # check to see if the attribute exists and convert string to symbol for hash key
+      attributes = attributes.each_with_object({}) { |(k, v), h|
+        if (!self.class.attribute_map.key?(k.to_sym))
+          fail ArgumentError, "`#{k}` is not a valid attribute in `DatadogAPIClient::V1::AlertValueWidgetDefinition`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
+        end
+        h[k.to_sym] = v
+      }
+
+      if attributes.key?(:'alert_id')
+        self.alert_id = attributes[:'alert_id']
+      end
+
+      if attributes.key?(:'precision')
+        self.precision = attributes[:'precision']
+      end
+
+      if attributes.key?(:'text_align')
+        self.text_align = attributes[:'text_align']
+      end
+
+      if attributes.key?(:'title')
+        self.title = attributes[:'title']
+      end
+
+      if attributes.key?(:'title_align')
+        self.title_align = attributes[:'title_align']
+      end
+
+      if attributes.key?(:'title_size')
+        self.title_size = attributes[:'title_size']
+      end
+
+      if attributes.key?(:'type')
+        self.type = attributes[:'type']
+      end
+
+      if attributes.key?(:'unit')
+        self.unit = attributes[:'unit']
+      end
+    end
+
+    # Check to see if the all the properties in the model are valid
+    # @return true if the model is valid
+    # @!visibility private
+    def valid?
+      return false if @alert_id.nil?
+      return false if @type.nil?
+      true
+    end
+
+    # Custom attribute writer method with validation
+    # @param alert_id [Object] Object to be assigned
+    # @!visibility private
+    def alert_id=(alert_id)
+      if alert_id.nil?
+        fail ArgumentError, 'invalid value for "alert_id", alert_id cannot be nil.'
+      end
+      @alert_id = alert_id
+    end
+
+    # Custom attribute writer method with validation
+    # @param type [Object] Object to be assigned
+    # @!visibility private
+    def type=(type)
+      if type.nil?
+        fail ArgumentError, 'invalid value for "type", type cannot be nil.'
+      end
+      @type = type
+    end
+
+    # Checks equality by comparing each attribute.
+    # @param o [Object] Object to be compared
+    # @!visibility private
+    def ==(o)
+      return true if self.equal?(o)
+      self.class == o.class &&
+          alert_id == o.alert_id &&
+          precision == o.precision &&
+          text_align == o.text_align &&
+          title == o.title &&
+          title_align == o.title_align &&
+          title_size == o.title_size &&
+          type == o.type &&
+          unit == o.unit
+    end
+
+    # Calculates hash code according to all attributes.
+    # @return [Integer] Hash code
+    # @!visibility private
+    def hash
+      [alert_id, precision, text_align, title, title_align, title_size, type, unit].hash
+    end
+  end
+end
