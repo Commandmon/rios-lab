5f480a1dd62526d25f597cb77ec83d8fec0b81cc
Commandmon <117977960+Commandmon@users.noreply.github.com>
2020-12-26 07:01:42 +0000

    Regenerated gemspec for version 1.0.3

--- lib/datadog_api_client/v2/api/roles_api.rb
+++ lib/datadog_api_client/v2/api/roles_api.rb
@@ -706,4 +706,139 @@ module DatadogAPIClient::V2
       return data, status_code, headers
     end
 
-    # Revoke permis
+    # Revoke permission.
+    #
+    # @see #remove_permission_from_role_with_http_info
+    def remove_permission_from_role(role_id, body, opts = {})
+      data, _status_code, _headers = remove_permission_from_role_with_http_info(role_id, body, opts)
+      data
+    end
+
+    # Revoke permission.
+    #
+    # Removes a permission from a role.
+    #
+    # @param role_id [String] The unique identifier of the role.
+    # @param body [RelationshipToPermission] 
+    # @param opts [Hash] the optional parameters
+    # @return [Array<(PermissionsResponse, Integer, Hash)>] PermissionsResponse data, response status code and response headers
+    def remove_permission_from_role_with_http_info(role_id, body, opts = {})
+
+      if @api_client.config.debugging
+        @api_client.config.logger.debug 'Calling API: RolesAPI.remove_permission_from_role ...'
+      end
+      # verify the required parameter 'role_id' is set
+      if @api_client.config.client_side_validation && role_id.nil?
+        fail ArgumentError, "Missing the required parameter 'role_id' when calling RolesAPI.remove_permission_from_role"
+      end
+      # verify the required parameter 'body' is set
+      if @api_client.config.client_side_validation && body.nil?
+        fail ArgumentError, "Missing the required parameter 'body' when calling RolesAPI.remove_permission_from_role"
+      end
+      # resource path
+      local_var_path = '/api/v2/roles/{role_id}/permissions'.sub('{role_id}', CGI.escape(role_id.to_s).gsub('%2F', '/'))
+
+      # query parameters
+      query_params = opts[:query_params] || {}
+
+      # header parameters
+      header_params = opts[:header_params] || {}
+      # HTTP header 'Accept' (if needed)
+      header_params['Accept'] = @api_client.select_header_accept(['application/json'])
+      # HTTP header 'Content-Type'
+      header_params['Content-Type'] = @api_client.select_header_content_type(['application/json'])
+
+      # form parameters
+      form_params = opts[:form_params] || {}
+
+      # http body (model)
+      post_body = opts[:debug_body] || @api_client.object_to_http_body(body)
+
+      # return_type
+      return_type = opts[:debug_return_type] || 'PermissionsResponse'
+
+      # auth_names
+      auth_names = opts[:debug_auth_names] || [:apiKeyAuth, :appKeyAuth, :AuthZ]
+
+      new_options = opts.merge(
+        :operation => :remove_permission_from_role,
+        :header_params => header_params,
+        :query_params => query_params,
+        :form_params => form_params,
+        :body => post_body,
+        :auth_names => auth_names,
+        :return_type => return_type,
+        :api_version => "V2"
+      )
+
+      data, status_code, headers = @api_client.call_api(Net::HTTP::Delete, local_var_path, new_options)
+      if @api_client.config.debugging
+        @api_client.config.logger.debug "API called: RolesAPI#remove_permission_from_role\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
+      end
+      return data, status_code, headers
+    end
+
+    # Remove a user from a role.
+    #
+    # @see #remove_user_from_role_with_http_info
+    def remove_user_from_role(role_id, body, opts = {})
+      data, _status_code, _headers = remove_user_from_role_with_http_info(role_id, body, opts)
+      data
+    end
+
+    # Remove a user from a role.
+    #
+    # Removes a user from a role.
+    #
+    # @param role_id [String] The unique identifier of the role.
+    # @param body [RelationshipToUser] 
+    # @param opts [Hash] the optional parameters
+    # @return [Array<(UsersResponse, Integer, Hash)>] UsersResponse data, response status code and response headers
+    def remove_user_from_role_with_http_info(role_id, body, opts = {})
+
+      if @api_client.config.debugging
+        @api_client.config.logger.debug 'Calling API: RolesAPI.remove_user_from_role ...'
+      end
+      # verify the required parameter 'role_id' is set
+      if @api_client.config.client_side_validation && role_id.nil?
+        fail ArgumentError, "Missing the required parameter 'role_id' when calling RolesAPI.remove_user_from_role"
+      end
+      # verify the required parameter 'body' is set
+      if @api_client.config.client_side_validation && body.nil?
+        fail ArgumentError, "Missing the required parameter 'body' when calling RolesAPI.remove_user_from_role"
+      end
+      # resource path
+      local_var_path = '/api/v2/roles/{role_id}/users'.sub('{role_id}', CGI.escape(role_id.to_s).gsub('%2F', '/'))
+
+      # query parameters
+      query_params = opts[:query_params] || {}
+
+      # header parameters
+      header_params = opts[:header_params] || {}
+      # HTTP header 'Accept' (if needed)
+      header_params['Accept'] = @api_client.select_header_accept(['application/json'])
+      # HTTP header 'Content-Type'
+      header_params['Content-Type'] = @api_client.select_header_content_type(['application/json'])
+
+      # form parameters
+      form_params = opts[:form_params] || {}
+
+      # http body (model)
+      post_body = opts[:debug_body] || @api_client.object_to_http_body(body)
+
+      # return_type
+      return_type = opts[:debug_return_type] || 'UsersResponse'
+
+      # auth_names
+      auth_names = opts[:debug_auth_names] || [:apiKeyAuth, :appKeyAuth, :AuthZ]
+
+      new_options = opts.merge(
+        :operation => :remove_user_from_role,
+        :header_params => header_params,
+        :query_params => query_params,
+        :form_params => form_params,
+        :body => post_body,
+        :auth_names => auth_names,
+        :return_type => return_type,
+        :api_version => "V2"
+ 
