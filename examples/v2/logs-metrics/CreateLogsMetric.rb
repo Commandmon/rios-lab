14e0388e0814685de043257d63bd4d0b6ab5dc8f
Commandmon <117977960+Commandmon@users.noreply.github.com>
2020-12-31 13:20:35 +0000

    Update clicktime.html

---
+++ examples/v2/logs-metrics/CreateLogsMetric.rb
@@ -0,0 +1,19 @@
+# Create a log-based metric returns "OK" response
+
+require "datadog_api_client"
+api_instance = DatadogAPIClient::V2::LogsMetricsAPI.new
+
+body = DatadogAPIClient::V2::LogsMetricCreateRequest.new({
+  data: DatadogAPIClient::V2::LogsMetricCreateData.new({
+    id: "ExampleCreatealogbasedmetricreturnsOKresponse",
+    type: DatadogAPIClient::V2::LogsMetricType::LOGS_METRICS,
+    attributes: DatadogAPIClient::V2::LogsMetricCreateAttributes.new({
+      compute: DatadogAPIClient::V2::LogsMetricCompute.new({
+        aggregation_type: DatadogAPIClient::V2::LogsMetricComputeAggregationType::DISTRIBUTION,
+        include_percentiles: true,
+        path: "@duration",
+      }),
+    }),
+  }),
+})
+p api_instance.create_logs_metric(body)
