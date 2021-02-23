722a1e05cc8efae1f8bed705165490bda0bda09f
Commandmon <117977960+Commandmon@users.noreply.github.com>
2021-02-23 06:42:02 +0000

    release: patch

--- lib/datadog_api_client/v2/api/rum_api.rb
+++ lib/datadog_api_client/v2/api/rum_api.rb
@@ -531,4 +531,90 @@ module DatadogAPIClient::V2
     # To use it you need to use a block: search_rum_events_with_pagination { |item| p item }
     #
     # @yield [RUMEvent] Paginated items
-    def s
+    def search_rum_events_with_pagination(body, opts = {})
+        api_version = "V2"
+        page_size = @api_client.get_attribute_from_path(body, "page.limit", 10)
+        @api_client.set_attribute_from_path(api_version, body, "page.limit", RUMSearchEventsRequest, page_size)
+        while true do
+            response = search_rum_events(body, opts)
+            @api_client.get_attribute_from_path(response, "data").each { |item| yield(item) }
+            if @api_client.get_attribute_from_path(response, "data").length < page_size
+              break
+            end
+            @api_client.set_attribute_from_path(api_version, body, "page.cursor", RUMSearchEventsRequest, @api_client.get_attribute_from_path(response, "meta.page.after"))
+        end
+    end
+
+    # Update a RUM application.
+    #
+    # @see #update_rum_application_with_http_info
+    def update_rum_application(id, body, opts = {})
+      data, _status_code, _headers = update_rum_application_with_http_info(id, body, opts)
+      data
+    end
+
+    # Update a RUM application.
+    #
+    # Update the RUM application with given ID in your organization.
+    #
+    # @param id [String] RUM application ID.
+    # @param body [RUMApplicationUpdateRequest] 
+    # @param opts [Hash] the optional parameters
+    # @return [Array<(RUMApplicationResponse, Integer, Hash)>] RUMApplicationResponse data, response status code and response headers
+    def update_rum_application_with_http_info(id, body, opts = {})
+
+      if @api_client.config.debugging
+        @api_client.config.logger.debug 'Calling API: RUMAPI.update_rum_application ...'
+      end
+      # verify the required parameter 'id' is set
+      if @api_client.config.client_side_validation && id.nil?
+        fail ArgumentError, "Missing the required parameter 'id' when calling RUMAPI.update_rum_application"
+      end
+      # verify the required parameter 'body' is set
+      if @api_client.config.client_side_validation && body.nil?
+        fail ArgumentError, "Missing the required parameter 'body' when calling RUMAPI.update_rum_application"
+      end
+      # resource path
+      local_var_path = '/api/v2/rum/applications/{id}'.sub('{id}', CGI.escape(id.to_s).gsub('%2F', '/'))
+
+      # query parameters
+      query_params = opts[:query_params] || {}
+
+      # header parameters
+      header_params = opts[:header_params] || {}
+      # HTTP header 'Accept' (if needed)
+      header_params['Accept'] = @api_client.select_header_accept(['application/json'])
+      # HTTP header 'Content-Type'
+      header_params['Content-Type'] = @api_client.select_header_content_type(['application/json'])
+
+      # form parameters
+      form_params = opts[:form_params] || {}
+
+      # http body (model)
+      post_body = opts[:debug_body] || @api_client.object_to_http_body(body)
+
+      # return_type
+      return_type = opts[:debug_return_type] || 'RUMApplicationResponse'
+
+      # auth_names
+      auth_names = opts[:debug_auth_names] || [:apiKeyAuth, :appKeyAuth]
+
+      new_options = opts.merge(
+        :operation => :update_rum_application,
+        :header_params => header_params,
+        :query_params => query_params,
+        :form_params => form_params,
+        :body => post_body,
+        :auth_names => auth_names,
+        :return_type => return_type,
+        :api_version => "V2"
+      )
+
+      data, status_code, headers = @api_client.call_api(Net::HTTP::Patch, local_var_path, new_options)
+      if @api_client.config.debugging
+        @api_client.config.logger.debug "API called: RUMAPI#update_rum_application\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
+      end
+      return data, status_code, headers
+    end
+  end
+end
