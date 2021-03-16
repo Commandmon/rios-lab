f66b28f5bad95cf4104f0915f98a21faddfeb28a
Commandmon <117977960+Commandmon@users.noreply.github.com>
2021-03-16 16:48:39 +0000

    Added travis.yml.

--- lib/datadog_api_client/v1/models/usage_summary_date.rb
+++ lib/datadog_api_client/v1/models/usage_summary_date.rb
@@ -103,4 +103,106 @@ module DatadogAPIClient::V1
     # Shows the 99th percentile of all Cloud Security Posture Management AWS hosts over all hours in the current date for all organizations.
     attr_accessor :cspm_aws_host_top99p
 
-    # Shows the 99th percentile of all Cloud Security Posture Ma
+    # Shows the 99th percentile of all Cloud Security Posture Management Azure hosts over all hours in the current date for all organizations.
+    attr_accessor :cspm_azure_host_top99p
+
+    # Shows the average number of Cloud Security Posture Management containers over all hours in the current date for all organizations.
+    attr_accessor :cspm_container_avg
+
+    # Shows the high-water mark of Cloud Security Posture Management containers over all hours in the current date for all organizations.
+    attr_accessor :cspm_container_hwm
+
+    # Shows the 99th percentile of all Cloud Security Posture Management GCP hosts over all hours in the current date for all organizations.
+    attr_accessor :cspm_gcp_host_top99p
+
+    # Shows the 99th percentile of all Cloud Security Posture Management hosts over all hours in the current date for all organizations.
+    attr_accessor :cspm_host_top99p
+
+    # Shows the average number of distinct custom metrics over all hours in the current date for all organizations.
+    attr_accessor :custom_ts_avg
+
+    # Shows the average of all distinct Cloud Workload Security containers over all hours in the current date for all organizations.
+    attr_accessor :cws_container_count_avg
+
+    # Shows the 99th percentile of all Cloud Workload Security hosts over all hours in the current date for all organizations.
+    attr_accessor :cws_host_top99p
+
+    # The date for the usage.
+    attr_accessor :date
+
+    # Shows the 99th percentile of all Database Monitoring hosts over all hours in the current date for all organizations.
+    attr_accessor :dbm_host_top99p
+
+    # Shows the average of all normalized Database Monitoring queries over all hours in the current date for all organizations.
+    attr_accessor :dbm_queries_count_avg
+
+    # Shows the high-watermark of all Fargate tasks over all hours in the current date for all organizations.
+    attr_accessor :fargate_tasks_count_avg
+
+    # Shows the average of all Fargate tasks over all hours in the current date for all organizations.
+    attr_accessor :fargate_tasks_count_hwm
+
+    # Shows the 99th percentile of all GCP hosts over all hours in the current date for all organizations.
+    attr_accessor :gcp_host_top99p
+
+    # Shows the 99th percentile of all Heroku dynos over all hours in the current date for all organizations.
+    attr_accessor :heroku_host_top99p
+
+    # Shows the high-water mark of incident management monthly active users over all hours in the current date for all organizations.
+    attr_accessor :incident_management_monthly_active_users_hwm
+
+    # Shows the sum of all log events indexed over all hours in the current date for all organizations.
+    attr_accessor :indexed_events_count_sum
+
+    # Shows the 99th percentile of all distinct infrastructure hosts over all hours in the current date for all organizations.
+    attr_accessor :infra_host_top99p
+
+    # Shows the sum of all log bytes ingested over all hours in the current date for all organizations.
+    attr_accessor :ingested_events_bytes_sum
+
+    # Shows the sum of all IoT devices over all hours in the current date for all organizations.
+    attr_accessor :iot_device_sum
+
+    # Shows the 99th percentile of all IoT devices over all hours in the current date all organizations.
+    attr_accessor :iot_device_top99p
+
+    # Shows the sum of all mobile lite sessions over all hours in the current date for all organizations.
+    attr_accessor :mobile_rum_lite_session_count_sum
+
+    # Shows the sum of all mobile RUM Sessions on Android over all hours in the current date for all organizations.
+    attr_accessor :mobile_rum_session_count_android_sum
+
+    # Shows the sum of all mobile RUM Sessions on Flutter over all hours in the current date for all organizations.
+    attr_accessor :mobile_rum_session_count_flutter_sum
+
+    # Shows the sum of all mobile RUM Sessions on iOS over all hours in the current date for all organizations.
+    attr_accessor :mobile_rum_session_count_ios_sum
+
+    # Shows the sum of all mobile RUM Sessions on React Native over all hours in the current date for all organizations.
+    attr_accessor :mobile_rum_session_count_reactnative_sum
+
+    # Shows the sum of all mobile RUM Sessions over all hours in the current date for all organizations
+    attr_accessor :mobile_rum_session_count_sum
+
+    # Shows the sum of all mobile RUM units over all hours in the current date for all organizations.
+    attr_accessor :mobile_rum_units_sum
+
+    # Shows the sum of all Network flows indexed over all hours in the current date for all organizations.
+    attr_accessor :netflow_indexed_events_count_sum
+
+    # Shows the 99th percentile of all distinct Networks hosts over all hours in the current date for all organizations.
+    attr_accessor :npm_host_top99p
+
+    # Sum of all observability pipelines bytes processed over all hours in the current date for the given org.
+    attr_accessor :observability_pipelines_bytes_processed_sum
+
+    # Sum of all online archived events over all hours in the current date for all organizations.
+    attr_accessor :online_archive_events_count_sum
+
+    # Shows the 99th percentile of APM hosts reported by the Datadog exporter for the OpenTelemetry Collector over all hours in the current date for all organizations.
+    attr_accessor :opentelemetry_apm_host_top99p
+
+    # Shows the 99th percentile of all hosts reported by the Datadog exporter for the OpenTelemetry Collector over all hours in the current date for all organizations.
+    attr_accessor :opentelemetry_host_top99p
+
+    # Organizations associated with
