27b2b8161b9e11fed18a544b8977146f741a993b
Commandmon <117977960+Commandmon@users.noreply.github.com>
2021-04-09 22:17:44 +0000

    Fix some translations

--- examples/v1/dashboards/CreateDashboard_798168180.rb
+++ examples/v1/dashboards/CreateDashboard_798168180.rb
@@ -18,4 +18,27 @@ body = DatadogAPIClient::V1::Dashboard.new({
             queries: [
               DatadogAPIClient::V1::FormulaAndFunctionApmDependencyStatsQueryDefinition.new({
                 primary_tag_value: "edge-eu1.prod.dog",
-                stat: DatadogAPIClient::V1::FormulaAndFunctionApmDependencyStatName::AVG_DURATION
+                stat: DatadogAPIClient::V1::FormulaAndFunctionApmDependencyStatName::AVG_DURATION,
+                resource_name: "DELETE FROM monitor_history.monitor_state_change_history WHERE org_id = ? AND monitor_id IN ? AND group = ?",
+                name: "query1",
+                service: "cassandra",
+                data_source: DatadogAPIClient::V1::FormulaAndFunctionApmDependencyStatsDataSource::APM_DEPENDENCY_STATS,
+                env: "ci",
+                primary_tag_name: "datacenter",
+                operation_name: "cassandra.query",
+              }),
+            ],
+          }),
+        ],
+      }),
+      layout: DatadogAPIClient::V1::WidgetLayout.new({
+        x: 0,
+        y: 0,
+        width: 4,
+        height: 4,
+      }),
+    }),
+  ],
+  layout_type: DatadogAPIClient::V1::DashboardLayoutType::ORDERED,
+})
+p api_instance.create_dashboard(body)
