b90248fbabfebcc5e028e1660e63f88a7b3dd3d7
Commandmon <117977960+Commandmon@users.noreply.github.com>
2021-05-04 19:55:39 +0000

    add test case

--- lib/datadog_api_client/v1/models/log_stream_widget_definition.rb
+++ lib/datadog_api_client/v1/models/log_stream_widget_definition.rb
@@ -79,4 +79,148 @@ module DatadogAPIClient::V1
         :'time' => :'time',
         :'title' => :'title',
         :'title_align' => :'title_align',
-        :'title_si
+        :'title_size' => :'title_size',
+        :'type' => :'type'
+      }
+    end
+
+    # Attribute type mapping.
+    # @!visibility private
+    def self.openapi_types
+      {
+        :'columns' => :'Array<String>',
+        :'indexes' => :'Array<String>',
+        :'logset' => :'String',
+        :'message_display' => :'WidgetMessageDisplay',
+        :'query' => :'String',
+        :'show_date_column' => :'Boolean',
+        :'show_message_column' => :'Boolean',
+        :'sort' => :'WidgetFieldSort',
+        :'time' => :'WidgetTime',
+        :'title' => :'String',
+        :'title_align' => :'WidgetTextAlign',
+        :'title_size' => :'String',
+        :'type' => :'LogStreamWidgetDefinitionType'
+      }
+    end
+
+    # Initializes the object
+    # @param attributes [Hash] Model attributes in the form of hash
+    # @!visibility private
+    def initialize(attributes = {})
+      if (!attributes.is_a?(Hash))
+        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V1::LogStreamWidgetDefinition` initialize method"
+      end
+
+      # check to see if the attribute exists and convert string to symbol for hash key
+      attributes = attributes.each_with_object({}) { |(k, v), h|
+        if (!self.class.attribute_map.key?(k.to_sym))
+          fail ArgumentError, "`#{k}` is not a valid attribute in `DatadogAPIClient::V1::LogStreamWidgetDefinition`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
+        end
+        h[k.to_sym] = v
+      }
+
+      if attributes.key?(:'columns')
+        if (value = attributes[:'columns']).is_a?(Array)
+          self.columns = value
+        end
+      end
+
+      if attributes.key?(:'indexes')
+        if (value = attributes[:'indexes']).is_a?(Array)
+          self.indexes = value
+        end
+      end
+
+      if attributes.key?(:'logset')
+        self.logset = attributes[:'logset']
+      end
+
+      if attributes.key?(:'message_display')
+        self.message_display = attributes[:'message_display']
+      end
+
+      if attributes.key?(:'query')
+        self.query = attributes[:'query']
+      end
+
+      if attributes.key?(:'show_date_column')
+        self.show_date_column = attributes[:'show_date_column']
+      end
+
+      if attributes.key?(:'show_message_column')
+        self.show_message_column = attributes[:'show_message_column']
+      end
+
+      if attributes.key?(:'sort')
+        self.sort = attributes[:'sort']
+      end
+
+      if attributes.key?(:'time')
+        self.time = attributes[:'time']
+      end
+
+      if attributes.key?(:'title')
+        self.title = attributes[:'title']
+      end
+
+      if attributes.key?(:'title_align')
+        self.title_align = attributes[:'title_align']
+      end
+
+      if attributes.key?(:'title_size')
+        self.title_size = attributes[:'title_size']
+      end
+
+      if attributes.key?(:'type')
+        self.type = attributes[:'type']
+      end
+    end
+
+    # Check to see if the all the properties in the model are valid
+    # @return true if the model is valid
+    # @!visibility private
+    def valid?
+      return false if @type.nil?
+      true
+    end
+
+    # Custom attribute writer method with validation
+    # @param type [Object] Object to be assigned
+    # @!visibility private
+    def type=(type)
+      if type.nil?
+        fail ArgumentError, 'invalid value for "type", type cannot be nil.'
+      end
+      @type = type
+    end
+
+    # Checks equality by comparing each attribute.
+    # @param o [Object] Object to be compared
+    # @!visibility private
+    def ==(o)
+      return true if self.equal?(o)
+      self.class == o.class &&
+          columns == o.columns &&
+          indexes == o.indexes &&
+          logset == o.logset &&
+          message_display == o.message_display &&
+          query == o.query &&
+          show_date_column == o.show_date_column &&
+          show_message_column == o.show_message_column &&
+          sort == o.sort &&
+          time == o.time &&
+          title == o.title &&
+          title_align == o.title_align &&
+          title_size == o.title_size &&
+          type == o.type
+    end
+
+    # Calculates hash code according to all attributes.
+    # @return [Integer] Hash code
+    # @!visibility private
+    def hash
+      [columns, indexes, logset, message_display, query, show_date_column, show_message_column, sort, time, title, title_align, title_size, type].hash
+    end
+  end
+end
