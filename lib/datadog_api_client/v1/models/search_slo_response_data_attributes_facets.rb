4d4e635d1fcc1109fe80c485d1400fdff3affb48
Commandmon <117977960+Commandmon@users.noreply.github.com>
2021-06-01 14:12:37 +0000

    i messed up

--- lib/datadog_api_client/v1/models/search_slo_response_data_attributes_facets.rb
+++ lib/datadog_api_client/v1/models/search_slo_response_data_attributes_facets.rb
@@ -68,4 +68,110 @@ module DatadogAPIClient::V1
     # @!visibility private
     def self.openapi_types
       {
-        :'all_tags' => :'Array<SearchSLOResponseDataAttributesFacetsObjectStri
+        :'all_tags' => :'Array<SearchSLOResponseDataAttributesFacetsObjectString>',
+        :'creator_name' => :'Array<SearchSLOResponseDataAttributesFacetsObjectString>',
+        :'env_tags' => :'Array<SearchSLOResponseDataAttributesFacetsObjectString>',
+        :'service_tags' => :'Array<SearchSLOResponseDataAttributesFacetsObjectString>',
+        :'slo_type' => :'Array<SearchSLOResponseDataAttributesFacetsObjectInt>',
+        :'target' => :'Array<SearchSLOResponseDataAttributesFacetsObjectInt>',
+        :'team_tags' => :'Array<SearchSLOResponseDataAttributesFacetsObjectString>',
+        :'timeframe' => :'Array<SearchSLOResponseDataAttributesFacetsObjectString>'
+      }
+    end
+
+    # Initializes the object
+    # @param attributes [Hash] Model attributes in the form of hash
+    # @!visibility private
+    def initialize(attributes = {})
+      if (!attributes.is_a?(Hash))
+        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V1::SearchSLOResponseDataAttributesFacets` initialize method"
+      end
+
+      # check to see if the attribute exists and convert string to symbol for hash key
+      attributes = attributes.each_with_object({}) { |(k, v), h|
+        if (!self.class.attribute_map.key?(k.to_sym))
+          fail ArgumentError, "`#{k}` is not a valid attribute in `DatadogAPIClient::V1::SearchSLOResponseDataAttributesFacets`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
+        end
+        h[k.to_sym] = v
+      }
+
+      if attributes.key?(:'all_tags')
+        if (value = attributes[:'all_tags']).is_a?(Array)
+          self.all_tags = value
+        end
+      end
+
+      if attributes.key?(:'creator_name')
+        if (value = attributes[:'creator_name']).is_a?(Array)
+          self.creator_name = value
+        end
+      end
+
+      if attributes.key?(:'env_tags')
+        if (value = attributes[:'env_tags']).is_a?(Array)
+          self.env_tags = value
+        end
+      end
+
+      if attributes.key?(:'service_tags')
+        if (value = attributes[:'service_tags']).is_a?(Array)
+          self.service_tags = value
+        end
+      end
+
+      if attributes.key?(:'slo_type')
+        if (value = attributes[:'slo_type']).is_a?(Array)
+          self.slo_type = value
+        end
+      end
+
+      if attributes.key?(:'target')
+        if (value = attributes[:'target']).is_a?(Array)
+          self.target = value
+        end
+      end
+
+      if attributes.key?(:'team_tags')
+        if (value = attributes[:'team_tags']).is_a?(Array)
+          self.team_tags = value
+        end
+      end
+
+      if attributes.key?(:'timeframe')
+        if (value = attributes[:'timeframe']).is_a?(Array)
+          self.timeframe = value
+        end
+      end
+    end
+
+    # Check to see if the all the properties in the model are valid
+    # @return true if the model is valid
+    # @!visibility private
+    def valid?
+      true
+    end
+
+    # Checks equality by comparing each attribute.
+    # @param o [Object] Object to be compared
+    # @!visibility private
+    def ==(o)
+      return true if self.equal?(o)
+      self.class == o.class &&
+          all_tags == o.all_tags &&
+          creator_name == o.creator_name &&
+          env_tags == o.env_tags &&
+          service_tags == o.service_tags &&
+          slo_type == o.slo_type &&
+          target == o.target &&
+          team_tags == o.team_tags &&
+          timeframe == o.timeframe
+    end
+
+    # Calculates hash code according to all attributes.
+    # @return [Integer] Hash code
+    # @!visibility private
+    def hash
+      [all_tags, creator_name, env_tags, service_tags, slo_type, target, team_tags, timeframe].hash
+    end
+  end
+end
