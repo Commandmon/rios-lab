07e650c0beb09e57be2d20e95c81cd8fea8c0a98
Commandmon <117977960+Commandmon@users.noreply.github.com>
2021-06-12 10:57:04 +0000

    pubsub

---
+++ examples/v1/service-level-objective-corrections/ListSLOCorrection.rb
@@ -0,0 +1,10 @@
+
+# Get all SLO corrections returns "OK" response
+
+require "datadog_api_client"
+api_instance = DatadogAPIClient::V1::ServiceLevelObjectiveCorrectionsAPI.new
+opts = {
+  offset: 1,
+  limit: 1,
+}
+p api_instance.list_slo_correction(opts)
