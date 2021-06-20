ad8f476542bbab336ab76d83575d1f5f31eca0c7
Commandmon <117977960+Commandmon@users.noreply.github.com>
2021-06-20 05:40:48 +0000

    -debug

--- lib/datadog_api_client/v1/models/usage_specified_custom_reports_page.rb
+++ lib/datadog_api_client/v1/models/usage_specified_custom_reports_page.rb
@@ -32,4 +32,60 @@ module DatadogAPIClient::V1
     # @!visibility private
     def self.attribute_map
       {
-        :'total_c
+        :'total_count' => :'total_count'
+      }
+    end
+
+    # Attribute type mapping.
+    # @!visibility private
+    def self.openapi_types
+      {
+        :'total_count' => :'Integer'
+      }
+    end
+
+    # Initializes the object
+    # @param attributes [Hash] Model attributes in the form of hash
+    # @!visibility private
+    def initialize(attributes = {})
+      if (!attributes.is_a?(Hash))
+        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V1::UsageSpecifiedCustomReportsPage` initialize method"
+      end
+
+      # check to see if the attribute exists and convert string to symbol for hash key
+      attributes = attributes.each_with_object({}) { |(k, v), h|
+        if (!self.class.attribute_map.key?(k.to_sym))
+          fail ArgumentError, "`#{k}` is not a valid attribute in `DatadogAPIClient::V1::UsageSpecifiedCustomReportsPage`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
+        end
+        h[k.to_sym] = v
+      }
+
+      if attributes.key?(:'total_count')
+        self.total_count = attributes[:'total_count']
+      end
+    end
+
+    # Check to see if the all the properties in the model are valid
+    # @return true if the model is valid
+    # @!visibility private
+    def valid?
+      true
+    end
+
+    # Checks equality by comparing each attribute.
+    # @param o [Object] Object to be compared
+    # @!visibility private
+    def ==(o)
+      return true if self.equal?(o)
+      self.class == o.class &&
+          total_count == o.total_count
+    end
+
+    # Calculates hash code according to all attributes.
+    # @return [Integer] Hash code
+    # @!visibility private
+    def hash
+      [total_count].hash
+    end
+  end
+end
