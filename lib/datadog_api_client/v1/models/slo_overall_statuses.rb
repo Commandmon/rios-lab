2dcaae5b44394d8d6675493a9b5a4cf182df5830
Commandmon <117977960+Commandmon@users.noreply.github.com>
2021-08-08 08:27:30 +0000

    Work on SL if: t+

--- lib/datadog_api_client/v1/models/slo_overall_statuses.rb
+++ lib/datadog_api_client/v1/models/slo_overall_statuses.rb
@@ -48,4 +48,137 @@ module DatadogAPIClient::V1
     attr_accessor :status
 
     # The target of the SLO.
-    attr_
+    attr_accessor :target
+
+    # The SLO time window options.
+    attr_accessor :timeframe
+
+    # Attribute mapping from ruby-style variable name to JSON key.
+    # @!visibility private
+    def self.attribute_map
+      {
+        :'error' => :'error',
+        :'error_budget_remaining' => :'error_budget_remaining',
+        :'indexed_at' => :'indexed_at',
+        :'raw_error_budget_remaining' => :'raw_error_budget_remaining',
+        :'span_precision' => :'span_precision',
+        :'state' => :'state',
+        :'status' => :'status',
+        :'target' => :'target',
+        :'timeframe' => :'timeframe'
+      }
+    end
+
+    # Attribute type mapping.
+    # @!visibility private
+    def self.openapi_types
+      {
+        :'error' => :'String',
+        :'error_budget_remaining' => :'Float',
+        :'indexed_at' => :'Integer',
+        :'raw_error_budget_remaining' => :'SLORawErrorBudgetRemaining',
+        :'span_precision' => :'Integer',
+        :'state' => :'SLOState',
+        :'status' => :'Float',
+        :'target' => :'Float',
+        :'timeframe' => :'SLOTimeframe'
+      }
+    end
+
+    # List of attributes with nullable: true
+    # @!visibility private
+    def self.openapi_nullable
+      Set.new([
+        :'error',
+        :'error_budget_remaining',
+        :'raw_error_budget_remaining',
+        :'span_precision',
+        :'status',
+      ])
+    end
+
+    # Initializes the object
+    # @param attributes [Hash] Model attributes in the form of hash
+    # @!visibility private
+    def initialize(attributes = {})
+      if (!attributes.is_a?(Hash))
+        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V1::SLOOverallStatuses` initialize method"
+      end
+
+      # check to see if the attribute exists and convert string to symbol for hash key
+      attributes = attributes.each_with_object({}) { |(k, v), h|
+        if (!self.class.attribute_map.key?(k.to_sym))
+          fail ArgumentError, "`#{k}` is not a valid attribute in `DatadogAPIClient::V1::SLOOverallStatuses`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
+        end
+        h[k.to_sym] = v
+      }
+
+      if attributes.key?(:'error')
+        self.error = attributes[:'error']
+      end
+
+      if attributes.key?(:'error_budget_remaining')
+        self.error_budget_remaining = attributes[:'error_budget_remaining']
+      end
+
+      if attributes.key?(:'indexed_at')
+        self.indexed_at = attributes[:'indexed_at']
+      end
+
+      if attributes.key?(:'raw_error_budget_remaining')
+        self.raw_error_budget_remaining = attributes[:'raw_error_budget_remaining']
+      end
+
+      if attributes.key?(:'span_precision')
+        self.span_precision = attributes[:'span_precision']
+      end
+
+      if attributes.key?(:'state')
+        self.state = attributes[:'state']
+      end
+
+      if attributes.key?(:'status')
+        self.status = attributes[:'status']
+      end
+
+      if attributes.key?(:'target')
+        self.target = attributes[:'target']
+      end
+
+      if attributes.key?(:'timeframe')
+        self.timeframe = attributes[:'timeframe']
+      end
+    end
+
+    # Check to see if the all the properties in the model are valid
+    # @return true if the model is valid
+    # @!visibility private
+    def valid?
+      true
+    end
+
+    # Checks equality by comparing each attribute.
+    # @param o [Object] Object to be compared
+    # @!visibility private
+    def ==(o)
+      return true if self.equal?(o)
+      self.class == o.class &&
+          error == o.error &&
+          error_budget_remaining == o.error_budget_remaining &&
+          indexed_at == o.indexed_at &&
+          raw_error_budget_remaining == o.raw_error_budget_remaining &&
+          span_precision == o.span_precision &&
+          state == o.state &&
+          status == o.status &&
+          target == o.target &&
+          timeframe == o.timeframe
+    end
+
+    # Calculates hash code according to all attributes.
+    # @return [Integer] Hash code
+    # @!visibility private
+    def hash
+      [error, error_budget_remaining, indexed_at, raw_error_budget_remaining, span_precision, state, status, target, timeframe].hash
+    end
+  end
+end
