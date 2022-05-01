17c8e014cf1f98ef074a0e09ed5c8f646c3b0163
Commandmon <117977960+Commandmon@users.noreply.github.com>
2022-05-01 06:35:36 +0000

    Reorganize.

--- lib/datadog_api_client/v2/models/metrics_scalar_query.rb
+++ lib/datadog_api_client/v2/models/metrics_scalar_query.rb
@@ -55,4 +55,100 @@ module DatadogAPIClient::V2
         :'aggregator' => :'MetricsAggregator',
         :'data_source' => :'MetricsDataSource',
         :'name' => :'String',
-        :'query' => :'S
+        :'query' => :'String'
+      }
+    end
+
+    # Initializes the object
+    # @param attributes [Hash] Model attributes in the form of hash
+    # @!visibility private
+    def initialize(attributes = {})
+      if (!attributes.is_a?(Hash))
+        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V2::MetricsScalarQuery` initialize method"
+      end
+
+      # check to see if the attribute exists and convert string to symbol for hash key
+      attributes = attributes.each_with_object({}) { |(k, v), h|
+        if (!self.class.attribute_map.key?(k.to_sym))
+          fail ArgumentError, "`#{k}` is not a valid attribute in `DatadogAPIClient::V2::MetricsScalarQuery`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
+        end
+        h[k.to_sym] = v
+      }
+
+      if attributes.key?(:'aggregator')
+        self.aggregator = attributes[:'aggregator']
+      end
+
+      if attributes.key?(:'data_source')
+        self.data_source = attributes[:'data_source']
+      end
+
+      if attributes.key?(:'name')
+        self.name = attributes[:'name']
+      end
+
+      if attributes.key?(:'query')
+        self.query = attributes[:'query']
+      end
+    end
+
+    # Check to see if the all the properties in the model are valid
+    # @return true if the model is valid
+    # @!visibility private
+    def valid?
+      return false if @aggregator.nil?
+      return false if @data_source.nil?
+      return false if @query.nil?
+      true
+    end
+
+    # Custom attribute writer method with validation
+    # @param aggregator [Object] Object to be assigned
+    # @!visibility private
+    def aggregator=(aggregator)
+      if aggregator.nil?
+        fail ArgumentError, 'invalid value for "aggregator", aggregator cannot be nil.'
+      end
+      @aggregator = aggregator
+    end
+
+    # Custom attribute writer method with validation
+    # @param data_source [Object] Object to be assigned
+    # @!visibility private
+    def data_source=(data_source)
+      if data_source.nil?
+        fail ArgumentError, 'invalid value for "data_source", data_source cannot be nil.'
+      end
+      @data_source = data_source
+    end
+
+    # Custom attribute writer method with validation
+    # @param query [Object] Object to be assigned
+    # @!visibility private
+    def query=(query)
+      if query.nil?
+        fail ArgumentError, 'invalid value for "query", query cannot be nil.'
+      end
+      @query = query
+    end
+
+    # Checks equality by comparing each attribute.
+    # @param o [Object] Object to be compared
+    # @!visibility private
+    def ==(o)
+      return true if self.equal?(o)
+      self.class == o.class &&
+          aggregator == o.aggregator &&
+          data_source == o.data_source &&
+          name == o.name &&
+          query == o.query
+    end
+
+    # Calculates hash code according to all attributes.
+    # @return [Integer] Hash code
+    # @!visibility private
+    def hash
+      [aggregator, data_source, name, query].hash
+    end
+  end
+end
