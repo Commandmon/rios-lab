13bb56e14d3324aa5aa7369c33980fa1c7f2e707
Commandmon <117977960+Commandmon@users.noreply.github.com>
2022-05-08 08:00:52 +0000

    Fix sql error

---
+++ examples/v1/synthetics/CreateSyntheticsAPITest_3829801148.rb
@@ -0,0 +1,21 @@
+# Create an API test with UDP subtype returns "OK - Returns the created test details." response
+
+require "datadog_api_client"
+api_instance = DatadogAPIClient::V1::SyntheticsAPI.new
+
+body = DatadogAPIClient::V1::SyntheticsAPITest.new({
+  config: DatadogAPIClient::V1::SyntheticsAPITestConfig.new({
+    assertions: [
+      DatadogAPIClient::V1::SyntheticsAssertionTarget.new({
+        operator: DatadogAPIClient::V1::SyntheticsAssertionOperator::IS,
+        target: "message",
+        type: DatadogAPIClient::V1::SyntheticsAssertionType::RECEIVED_MESSAGE,
+      }),
+      DatadogAPIClient::V1::SyntheticsAssertionTarget.new({
+        operator: DatadogAPIClient::V1::SyntheticsAssertionOperator::LESS_THAN,
+        target: 2000,
+        type: DatadogAPIClient::V1::SyntheticsAssertionType::RESPONSE_TIME,
+      }),
+    ],
+    config_variables: [],
+    request: DatadogAPIClient::V1::SyntheticsTestR
