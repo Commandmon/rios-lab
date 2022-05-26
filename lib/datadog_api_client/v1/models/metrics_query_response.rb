33f081a9f0dd45ff705040f072aa54ccd051e961
Commandmon <117977960+Commandmon@users.noreply.github.com>
2022-05-26 10:59:37 +0000

    drafts

--- lib/datadog_api_client/v1/models/metrics_query_response.rb
+++ lib/datadog_api_client/v1/models/metrics_query_response.rb
@@ -69,4 +69,107 @@ module DatadogAPIClient::V1
     end
 
     # Attribute type mapping.
-    # @!visibi
+    # @!visibility private
+    def self.openapi_types
+      {
+        :'error' => :'String',
+        :'from_date' => :'Integer',
+        :'group_by' => :'Array<String>',
+        :'message' => :'String',
+        :'query' => :'String',
+        :'res_type' => :'String',
+        :'series' => :'Array<MetricsQueryMetadata>',
+        :'status' => :'String',
+        :'to_date' => :'Integer'
+      }
+    end
+
+    # Initializes the object
+    # @param attributes [Hash] Model attributes in the form of hash
+    # @!visibility private
+    def initialize(attributes = {})
+      if (!attributes.is_a?(Hash))
+        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V1::MetricsQueryResponse` initialize method"
+      end
+
+      # check to see if the attribute exists and convert string to symbol for hash key
+      attributes = attributes.each_with_object({}) { |(k, v), h|
+        if (!self.class.attribute_map.key?(k.to_sym))
+          fail ArgumentError, "`#{k}` is not a valid attribute in `DatadogAPIClient::V1::MetricsQueryResponse`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
+        end
+        h[k.to_sym] = v
+      }
+
+      if attributes.key?(:'error')
+        self.error = attributes[:'error']
+      end
+
+      if attributes.key?(:'from_date')
+        self.from_date = attributes[:'from_date']
+      end
+
+      if attributes.key?(:'group_by')
+        if (value = attributes[:'group_by']).is_a?(Array)
+          self.group_by = value
+        end
+      end
+
+      if attributes.key?(:'message')
+        self.message = attributes[:'message']
+      end
+
+      if attributes.key?(:'query')
+        self.query = attributes[:'query']
+      end
+
+      if attributes.key?(:'res_type')
+        self.res_type = attributes[:'res_type']
+      end
+
+      if attributes.key?(:'series')
+        if (value = attributes[:'series']).is_a?(Array)
+          self.series = value
+        end
+      end
+
+      if attributes.key?(:'status')
+        self.status = attributes[:'status']
+      end
+
+      if attributes.key?(:'to_date')
+        self.to_date = attributes[:'to_date']
+      end
+    end
+
+    # Check to see if the all the properties in the model are valid
+    # @return true if the model is valid
+    # @!visibility private
+    def valid?
+      true
+    end
+
+    # Checks equality by comparing each attribute.
+    # @param o [Object] Object to be compared
+    # @!visibility private
+    def ==(o)
+      return true if self.equal?(o)
+      self.class == o.class &&
+          error == o.error &&
+          from_date == o.from_date &&
+          group_by == o.group_by &&
+          message == o.message &&
+          query == o.query &&
+          res_type == o.res_type &&
+          series == o.series &&
+          status == o.status &&
+          to_date == o.to_date
+    end
+
+    # Calculates hash code according to all attributes.
+    # @return [Integer] Hash code
+    # @!visibility private
+    def hash
+      [error, from_date, group_by, message, query, res_type, series, status, to_date].hash
+    end
+  end
+end
