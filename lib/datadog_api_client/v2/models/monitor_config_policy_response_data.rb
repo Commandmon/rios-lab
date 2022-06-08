5ccae0a09553a7032267ffc59b33508472eeda03
Commandmon <117977960+Commandmon@users.noreply.github.com>
2022-06-08 20:42:40 +0000

    try again.

--- lib/datadog_api_client/v2/models/monitor_config_policy_response_data.rb
+++ lib/datadog_api_client/v2/models/monitor_config_policy_response_data.rb
@@ -35,4 +35,77 @@ module DatadogAPIClient::V2
     attr_accessor :type
 
     # Attribute mapping from ruby-style variable name to JSON key.
-    
+    # @!visibility private
+    def self.attribute_map
+      {
+        :'attributes' => :'attributes',
+        :'id' => :'id',
+        :'type' => :'type'
+      }
+    end
+
+    # Attribute type mapping.
+    # @!visibility private
+    def self.openapi_types
+      {
+        :'attributes' => :'MonitorConfigPolicyAttributeResponse',
+        :'id' => :'String',
+        :'type' => :'MonitorConfigPolicyResourceType'
+      }
+    end
+
+    # Initializes the object
+    # @param attributes [Hash] Model attributes in the form of hash
+    # @!visibility private
+    def initialize(attributes = {})
+      if (!attributes.is_a?(Hash))
+        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V2::MonitorConfigPolicyResponseData` initialize method"
+      end
+
+      # check to see if the attribute exists and convert string to symbol for hash key
+      attributes = attributes.each_with_object({}) { |(k, v), h|
+        if (!self.class.attribute_map.key?(k.to_sym))
+          fail ArgumentError, "`#{k}` is not a valid attribute in `DatadogAPIClient::V2::MonitorConfigPolicyResponseData`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
+        end
+        h[k.to_sym] = v
+      }
+
+      if attributes.key?(:'attributes')
+        self.attributes = attributes[:'attributes']
+      end
+
+      if attributes.key?(:'id')
+        self.id = attributes[:'id']
+      end
+
+      if attributes.key?(:'type')
+        self.type = attributes[:'type']
+      end
+    end
+
+    # Check to see if the all the properties in the model are valid
+    # @return true if the model is valid
+    # @!visibility private
+    def valid?
+      true
+    end
+
+    # Checks equality by comparing each attribute.
+    # @param o [Object] Object to be compared
+    # @!visibility private
+    def ==(o)
+      return true if self.equal?(o)
+      self.class == o.class &&
+          attributes == o.attributes &&
+          id == o.id &&
+          type == o.type
+    end
+
+    # Calculates hash code according to all attributes.
+    # @return [Integer] Hash code
+    # @!visibility private
+    def hash
+      [attributes, id, type].hash
+    end
+  end
+end
