6dace41cc16ee5d66dad8b7c90f043f92053bedf
Commandmon <117977960+Commandmon@users.noreply.github.com>
2022-07-05 14:48:44 +0000

    Added standard .editorconfig file

--- examples/v2/security-monitoring/CreateSecurityMonitoringRule_1092490364.rb
+++ examples/v2/security-monitoring/CreateSecurityMonitoringRule_1092490364.rb
@@ -5,4 +5,37 @@ api_instance = DatadogAPIClient::V2::SecurityMonitoringAPI.new
 
 body = DatadogAPIClient::V2::CloudConfigurationRuleCreatePayload.new({
   type: DatadogAPIClient::V2::CloudConfigurationRuleType::CLOUD_CONFIGURATION,
-  name: "Example-Create_a_cloud_co
+  name: "Example-Create_a_cloud_configuration_rule_returns_OK_response_cloud",
+  is_enabled: false,
+  cases: [
+    DatadogAPIClient::V2::CloudConfigurationRuleCaseCreate.new({
+      status: DatadogAPIClient::V2::SecurityMonitoringRuleSeverity::INFO,
+      notifications: [
+        "channel",
+      ],
+    }),
+  ],
+  options: DatadogAPIClient::V2::CloudConfigurationRuleOptions.new({
+    compliance_rule_options: DatadogAPIClient::V2::CloudConfigurationComplianceRuleOptions.new({
+      resource_type: "gcp_compute_disk",
+      complex_rule: false,
+      rego_rule: DatadogAPIClient::V2::CloudConfigurationRegoRule.new({
+        policy: 'package datadog\n',
+        resource_types: [
+          "gcp_compute_disk",
+        ],
+      }),
+    }),
+  }),
+  message: "ddd",
+  tags: [
+    "my:tag",
+  ],
+  compliance_signal_options: DatadogAPIClient::V2::CloudConfigurationRuleComplianceSignalOptions.new({
+    user_activation_status: true,
+    user_group_by_fields: [
+      "@account_id",
+    ],
+  }),
+})
+p api_instance.create_security_monitoring_rule(body)
