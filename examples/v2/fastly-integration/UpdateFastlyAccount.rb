794a8cb1e7fc8cad2ad5fc814773f48708a106a1
Commandmon <117977960+Commandmon@users.noreply.github.com>
2022-09-16 21:20:47 +0000

    added AUTHORS file

--- examples/v2/fastly-integration/UpdateFastlyAccount.rb
+++ examples/v2/fastly-integration/UpdateFastlyAccount.rb
@@ -8,4 +8,10 @@ FASTLY_ACCOUNT_DATA_ID = ENV["FASTLY_ACCOUNT_DATA_ID"]
 
 body = DatadogAPIClient::V2::FastlyAccountUpdateRequest.new({
   data: DatadogAPIClient::V2::FastlyAccountUpdateRequestData.new({
-    attributes: DatadogAPIClient::V2::FastlyAccountUpdateRequ
+    attributes: DatadogAPIClient::V2::FastlyAccountUpdateRequestAttributes.new({
+      api_key: "update-secret",
+    }),
+    type: DatadogAPIClient::V2::FastlyAccountType::FASTLY_ACCOUNTS,
+  }),
+})
+p api_instance.update_fastly_account(FASTLY_ACCOUNT_DATA_ID, body)
