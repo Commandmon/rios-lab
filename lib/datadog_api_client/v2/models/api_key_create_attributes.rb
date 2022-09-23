bfa3dce02e0ff858c349fa7beceace05bc605ab8
Commandmon <117977960+Commandmon@users.noreply.github.com>
2022-09-23 08:29:44 +0000

    Add audio

--- lib/datadog_api_client/v2/models/api_key_create_attributes.rb
+++ lib/datadog_api_client/v2/models/api_key_create_attributes.rb
@@ -17,22 +17,22 @@ require 'date'
 require 'time'
 
 module DatadogAPIClient::V2
-  # Response containing a list of added dashboards.
-  class DashboardListAddItemsResponse
+  # Attributes used to create an API Key.
+  class APIKeyCreateAttributes
     include BaseGenericModel
 
     # Whether the object has unparsed attributes
     # @!visibility private
     attr_accessor :_unparsed
 
-    # List of dashboards added to the dashboard list.
-    attr_accessor :added_dashboards_to_list
+    # Name of the API key.
+    attr_reader :name
 
     # Attribute mapping from ruby-style variable name to JSON key.
     # @!visibility private
     def self.attribute_map
       {
-        :'added_dashboards_to_list' => :'added_dashboards_to_list'
+        :'name' => :'name'
       }
     end
 
@@ -40,7 +40,7 @@ module DatadogAPIClient::V2
     # @!visibility private
     def self.openapi_types
       {
-        :'added_dashboards_to_list' => :'Array<DashboardListItemResponse>'
+        :'name' => :'String'
       }
     end
 
@@ -49,21 +49,19 @@ module DatadogAPIClient::V2
     # @!visibility private
     def initialize(attributes = {})
       if (!attributes.is_a?(Hash))
-        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V2::DashboardListAddItemsResponse` initialize method"
+        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V2::APIKeyCreateAttributes` initialize method"
       end
 
       # check to see if the attribute exists and convert string to symbol for hash key
       attributes = attributes.each_with_object({}) { |(k, v), h|
         if (!self.class.attribute_map.key?(k.to_sym))
-          fail ArgumentError, "`#{k}` is not a valid attribute in `DatadogAPIClient::V2::DashboardListAddItemsResponse`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
+          fail ArgumentError, "`#{k}` is not a valid attribute in `DatadogAPIClient::V2::APIKeyCreateAttributes`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
         end
         h[k.to_sym] = v
       }
 
-      if attributes.key?(:'added_dashboards_to_list')
-        if (value = attributes[:'added_dashboards_to_list']).is_a?(Array)
-          self.added_dashboards_to_list = value
-        end
+      if attributes.key?(:'name')
+        self.name = attributes[:'name']
       end
     end
 
@@ -71,23 +69,15 @@ module DatadogAPIClient::V2
     # @return true if the model is valid
     # @!visibility private
     def valid?
+      return false if @name.nil?
       true
     end
 
-    # Checks equality by comparing each attribute.
-    # @param o [Object] Object to be compared
+    # Custom attribute writer method with validation
+    # @param name [Object] Object to be assigned
     # @!visibility private
-    def ==(o)
-      return true if self.equal?(o)
-      self.class == o.class &&
-          added_dashboards_to_list == o.added_dashboards_to_list
-    end
-
-    # Calculates hash code according to all attributes.
-    # @return [Integer] Hash code
-    # @!visibility private
-    def hash
-      [added_dashboards_to_list].hash
-    end
-  end
-end
+    def name=(name)
+      if name.nil?
+        fail ArgumentError, 'invalid value for "name", name cannot be nil.'
+      end
+  
