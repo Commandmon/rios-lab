477c9ba248e6e4692e71afe6d7a582ce99b4fa45
Commandmon <117977960+Commandmon@users.noreply.github.com>
2022-10-02 08:02:49 +0000

    updated version history

--- lib/datadog_api_client/v1/models/host_map_request.rb
+++ lib/datadog_api_client/v1/models/host_map_request.rb
@@ -70,3 +70,103 @@ module DatadogAPIClient::V1
     end
 
     # Attribute type mapping.
+    # @!visibility private
+    def self.openapi_types
+      {
+        :'apm_query' => :'LogQueryDefinition',
+        :'event_query' => :'LogQueryDefinition',
+        :'log_query' => :'LogQueryDefinition',
+        :'network_query' => :'LogQueryDefinition',
+        :'process_query' => :'ProcessQueryDefinition',
+        :'profile_metrics_query' => :'LogQueryDefinition',
+        :'q' => :'String',
+        :'rum_query' => :'LogQueryDefinition',
+        :'security_query' => :'LogQueryDefinition'
+      }
+    end
+
+    # Initializes the object
+    # @param attributes [Hash] Model attributes in the form of hash
+    # @!visibility private
+    def initialize(attributes = {})
+      if (!attributes.is_a?(Hash))
+        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V1::HostMapRequest` initialize method"
+      end
+
+      # check to see if the attribute exists and convert string to symbol for hash key
+      attributes = attributes.each_with_object({}) { |(k, v), h|
+        if (!self.class.attribute_map.key?(k.to_sym))
+          fail ArgumentError, "`#{k}` is not a valid attribute in `DatadogAPIClient::V1::HostMapRequest`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
+        end
+        h[k.to_sym] = v
+      }
+
+      if attributes.key?(:'apm_query')
+        self.apm_query = attributes[:'apm_query']
+      end
+
+      if attributes.key?(:'event_query')
+        self.event_query = attributes[:'event_query']
+      end
+
+      if attributes.key?(:'log_query')
+        self.log_query = attributes[:'log_query']
+      end
+
+      if attributes.key?(:'network_query')
+        self.network_query = attributes[:'network_query']
+      end
+
+      if attributes.key?(:'process_query')
+        self.process_query = attributes[:'process_query']
+      end
+
+      if attributes.key?(:'profile_metrics_query')
+        self.profile_metrics_query = attributes[:'profile_metrics_query']
+      end
+
+      if attributes.key?(:'q')
+        self.q = attributes[:'q']
+      end
+
+      if attributes.key?(:'rum_query')
+        self.rum_query = attributes[:'rum_query']
+      end
+
+      if attributes.key?(:'security_query')
+        self.security_query = attributes[:'security_query']
+      end
+    end
+
+    # Check to see if the all the properties in the model are valid
+    # @return true if the model is valid
+    # @!visibility private
+    def valid?
+      true
+    end
+
+    # Checks equality by comparing each attribute.
+    # @param o [Object] Object to be compared
+    # @!visibility private
+    def ==(o)
+      return true if self.equal?(o)
+      self.class == o.class &&
+          apm_query == o.apm_query &&
+          event_query == o.event_query &&
+          log_query == o.log_query &&
+          network_query == o.network_query &&
+          process_query == o.process_query &&
+          profile_metrics_query == o.profile_metrics_query &&
+          q == o.q &&
+          rum_query == o.rum_query &&
+          security_query == o.security_query
+    end
+
+    # Calculates hash code according to all attributes.
+    # @return [Integer] Hash code
+    # @!visibility private
+    def hash
+      [apm_query, event_query, log_query, network_query, process_query, profile_metrics_query, q, rum_query, security_query].hash
+    end
+  end
+end
