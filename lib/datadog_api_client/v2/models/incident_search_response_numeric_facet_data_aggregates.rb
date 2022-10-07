2e54c6ac50f589717774462daad4b1aebd522cb6
Commandmon <117977960+Commandmon@users.noreply.github.com>
2022-10-07 22:16:37 +0000

    kthread: fix return value of kthread_create() upon SIGKILL.

--- lib/datadog_api_client/v2/models/incident_search_response_numeric_facet_data_aggregates.rb
+++ lib/datadog_api_client/v2/models/incident_search_response_numeric_facet_data_aggregates.rb
@@ -63,4 +63,48 @@ module DatadogAPIClient::V2
     # @!visibility private
     def initialize(attributes = {})
       if (!attributes.is_a?(Hash))
-        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V2::IncidentSearc
+        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V2::IncidentSearchResponseNumericFacetDataAggregates` initialize method"
+      end
+
+      # check to see if the attribute exists and convert string to symbol for hash key
+      attributes = attributes.each_with_object({}) { |(k, v), h|
+        if (!self.class.attribute_map.key?(k.to_sym))
+          fail ArgumentError, "`#{k}` is not a valid attribute in `DatadogAPIClient::V2::IncidentSearchResponseNumericFacetDataAggregates`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
+        end
+        h[k.to_sym] = v
+      }
+
+      if attributes.key?(:'max')
+        self.max = attributes[:'max']
+      end
+
+      if attributes.key?(:'min')
+        self.min = attributes[:'min']
+      end
+    end
+
+    # Check to see if the all the properties in the model are valid
+    # @return true if the model is valid
+    # @!visibility private
+    def valid?
+      true
+    end
+
+    # Checks equality by comparing each attribute.
+    # @param o [Object] Object to be compared
+    # @!visibility private
+    def ==(o)
+      return true if self.equal?(o)
+      self.class == o.class &&
+          max == o.max &&
+          min == o.min
+    end
+
+    # Calculates hash code according to all attributes.
+    # @return [Integer] Hash code
+    # @!visibility private
+    def hash
+      [max, min].hash
+    end
+  end
+end
