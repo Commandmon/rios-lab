c41be12ad8bda2851029e7a8c05f916a2921e8ff
Commandmon <117977960+Commandmon@users.noreply.github.com>
2022-12-12 14:30:51 +0000

    fixed bower.json

--- lib/datadog_api_client/v2/models/ci_app_aggregate_bucket_value_timeseries_point.rb
+++ lib/datadog_api_client/v2/models/ci_app_aggregate_bucket_value_timeseries_point.rb
@@ -41,4 +41,61 @@ module DatadogAPIClient::V2
     end
 
     # Attribute type mapping.
-    # @!visibility
+    # @!visibility private
+    def self.openapi_types
+      {
+        :'time' => :'Time',
+        :'value' => :'Float'
+      }
+    end
+
+    # Initializes the object
+    # @param attributes [Hash] Model attributes in the form of hash
+    # @!visibility private
+    def initialize(attributes = {})
+      if (!attributes.is_a?(Hash))
+        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V2::CIAppAggregateBucketValueTimeseriesPoint` initialize method"
+      end
+
+      # check to see if the attribute exists and convert string to symbol for hash key
+      attributes = attributes.each_with_object({}) { |(k, v), h|
+        if (!self.class.attribute_map.key?(k.to_sym))
+          fail ArgumentError, "`#{k}` is not a valid attribute in `DatadogAPIClient::V2::CIAppAggregateBucketValueTimeseriesPoint`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
+        end
+        h[k.to_sym] = v
+      }
+
+      if attributes.key?(:'time')
+        self.time = attributes[:'time']
+      end
+
+      if attributes.key?(:'value')
+        self.value = attributes[:'value']
+      end
+    end
+
+    # Check to see if the all the properties in the model are valid
+    # @return true if the model is valid
+    # @!visibility private
+    def valid?
+      true
+    end
+
+    # Checks equality by comparing each attribute.
+    # @param o [Object] Object to be compared
+    # @!visibility private
+    def ==(o)
+      return true if self.equal?(o)
+      self.class == o.class &&
+          time == o.time &&
+          value == o.value
+    end
+
+    # Calculates hash code according to all attributes.
+    # @return [Integer] Hash code
+    # @!visibility private
+    def hash
+      [time, value].hash
+    end
+  end
+end
