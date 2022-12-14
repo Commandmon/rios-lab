bb6060dc0c53f854fc74b5ed8c04f6532bebef3d
Commandmon <117977960+Commandmon@users.noreply.github.com>
2022-12-14 10:52:47 +0000

    updated titanic page

---
+++ examples/v1/key-management/UpdateApplicationKey.rb
@@ -0,0 +1,10 @@
+
+# Edit an application key returns "OK" response
+
+require "datadog_api_client"
+api_instance = DatadogAPIClient::V1::KeyManagementAPI.new
+
+body = DatadogAPIClient::V1::ApplicationKey.new({
+  name: "example user",
+})
+p api_instance.update_application_key("key", body)
